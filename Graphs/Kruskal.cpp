6dc755ca0a8b4cf2e0c2a7775b83168d38bcc13d
azell003 <azell003@fiu.edu>
2015-04-04 21:57:19 -0400

    Kruskal Updated

--- Graphs/Kruskal.cpp
+++ Graphs/Kruskal.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 #include <algorithm>
+using namespace std;
 
 const int N = 1000000;
 
 int p[N];
 pair<int, pair<int, int> >edgeList[N];
 
-int getp(int i)
+int getp(int x)
 {
-	return (p[i] == p[p[i]] ? p[i] : p[i] = getp(p[i]));
+	return (p[x] == x ? x : p[x] = getp(p[x]));
 }
 
 int main()
 {
 	int e, v, ans = 0;
-	scanf("%d%d", &v, &e);
+	cin >> v >> e;
 	for(int i = 0; i < e; i++)
-		scanf("%d%d%d", &edgeList[i].second.first, &edgeList[i].second.second, &edgeList[i].first);
+		cin >> edgeList[i].second.first >> edgeList[i].second.second >> edgeList[i].first;
 
 	sort(edgeList, edgeList + e);
 
